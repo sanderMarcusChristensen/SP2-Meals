7042fd2ef1b6e58affd7253a6ccb8207437ebe9f
sanderMarcusChristensen <144114615+sanderMarcusChristensen@users.noreply.github.com>
2024-10-22 12:04:50 +0200

    IngredientsController update

--- src/main/java/dat/routes/IngredientsRoute.java
+++ src/main/java/dat/routes/IngredientsRoute.java
@@ -1,17 +1,24 @@
 package dat.routes;
 
+import dat.config.HibernateConfig;
 import dat.controller.impl.IngredientsController;
+import dat.daos.impl.IngredientsDAO;
 import dat.security.utils.Role;
 import io.javalin.apibuilder.EndpointGroup;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
 
 import static io.javalin.apibuilder.ApiBuilder.*;
 import static io.javalin.apibuilder.ApiBuilder.delete;
 
 public class IngredientsRoute {
 
-    private final IngredientsController ingredientsController = new IngredientsController();
 
-    protected EndpointGroup getIngredientsEndpointGroup() {
+    private final EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory("meals");
+    private final IngredientsDAO dao = new IngredientsDAO(emf);
+    private final IngredientsController ingredientsController = new IngredientsController(dao);
+
+    protected EndpointGroup getIngredientsRoutes() {
         return () -> {
 
             post("/", ingredientsController::create, Role.ADMIN);
