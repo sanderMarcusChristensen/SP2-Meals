ded22fc42cfb13ced34df53322048f12c9bcc42f
sanderMarcusChristensen <144114615+sanderMarcusChristensen@users.noreply.github.com>
2024-10-22 12:04:28 +0200

    Routes update

--- src/main/java/dat/routes/Routes.java
+++ src/main/java/dat/routes/Routes.java
@@ -7,11 +7,13 @@ import static io.javalin.apibuilder.ApiBuilder.path;
 public class Routes {
 
     private final MealRoute mealRoutes = new MealRoute();
+    private final IngredientsRoute ingredientsRoutes = new IngredientsRoute();
 
     public EndpointGroup getApiRoutes() {
 
         return () -> {
             path("/meals", mealRoutes.getMealRoutes());
+            path("ingredients", ingredientsRoutes.getIngredientsRoutes());
         };
     }
 }
