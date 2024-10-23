2d622e9483c83a352752a1512aafeb9400441a77
sanderMarcusChristensen <144114615+sanderMarcusChristensen@users.noreply.github.com>
2024-10-23 11:30:43 +0200

    dev.http updated

---
+++ src/test/java/dat/routes/IngredientsRouteTest.java
@@ -0,0 +1,22 @@
+package dat.routes;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IngredientsRouteTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getIngredientsRoutes() {
+    }
+}
